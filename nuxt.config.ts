d8662be004a85c1011d97cc0cf62adb640c1c3b5
mamaev <mamaev@myservices.digital>
2026-05-05 16:49:00 +0300

    chore: настроить базовую конфигурацию Nuxt для этапа 1

---
+++ nuxt.config.ts
@@ -0,0 +1,27 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: ["@pinia/nuxt", "@nuxt/eslint"],
+  css: ["~/app/assets/styles/global.css"],
+  typescript: {
+    strict: true,
+    typeCheck: true,
+  },
+  sourcemap: {
+    client: true,
+    server: true,
+  },
+  runtimeConfig: {
+    rss: {
+      mosUrl: "https://www.mos.ru/rss",
+      lentaUrl: "https://lenta.ru/rss",
+    },
+    public: {
+      rssDefaults: {
+        mosUrl: "https://www.mos.ru/rss",
+        lentaUrl: "https://lenta.ru/rss",
+      },
+    },
+  },
+});
